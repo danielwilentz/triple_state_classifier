b3111c17e8e579597378e9f9f4942c8af45d0a3d
danielwilentz <dan@osaro.com>
2018-10-30 22:02:12 -0700

    added two new models and made some changes to train.py

--- train_models/train.py
+++ train_models/train.py
@@ -113,7 +113,7 @@ def train_model(model, criterion, optimizer, scheduler, path, num_epochs=25):
 
     # load best model weights
     model.load_state_dict(best_model_wts)
-    torch.save(model.state_dict(), path + '/ft_model_2.pt')
+    torch.save(model.state_dict(), path + '/ft_adam_model_1.pt')
     return model
 
 
@@ -215,21 +215,22 @@ model_ft = model_ft.to(device)
 criterion = nn.CrossEntropyLoss()
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+optimizer_ft = optim.Adam(model_ft.parameters())
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 # train model
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       path, num_epochs=30)
+                       path, num_epochs=35)
 
 
 # # ConvNet as a fixed feature extractor
 # model_conv = torchvision.models.resnet18(pretrained=True)
 # for param in model_conv.parameters():
 #     param.requires_grad = False
-
+#
 # # Parameters of newly constructed modules have requires_grad=True by default
 # num_ftrs = model_conv.fc.in_features
 # model_conv.fc = nn.Linear(num_ftrs, 3)
